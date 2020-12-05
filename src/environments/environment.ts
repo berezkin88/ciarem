dc51931916587c3097e2d9c95f73901e877a155c
berezkin88 <berezkin05@gmail.com>
2020-12-05 15:59:40 +0200

    add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC1fWmdajKoFhfoqXTpOCzLVqE3U6NK_ow",
+    authDomain: "fir-demo-355c7.firebaseapp.com",
+    databaseURL: "https://fir-demo-355c7.firebaseio.com",
+    projectId: "fir-demo-355c7",
+    storageBucket: "fir-demo-355c7.appspot.com",
+    messagingSenderId: "459054369572",
+    appId: "1:459054369572:web:d38d076e1212268c7441b1"
+  }
 };
 
 /*
