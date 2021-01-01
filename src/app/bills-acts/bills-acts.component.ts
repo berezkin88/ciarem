721adff4501e4801946b701588fd59023976dd65
berezkin88 <berezkin05@gmail.com>
2021-01-01 14:31:24 +0200

    complete static table

--- src/app/bills-acts/bills-acts.component.ts
+++ src/app/bills-acts/bills-acts.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCaretUp,
+  faClipboardCheck,
+  faHourglassHalf,
+  faExclamationTriangle,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-bills-acts',
   templateUrl: './bills-acts.component.html',
-  styleUrls: ['./bills-acts.component.sass']
+  styleUrls: ['./bills-acts.component.sass'],
 })
 export class BillsActsComponent {
   up = faCaretUp;
+  complete = faClipboardCheck;
+  pending = faHourglassHalf;
+  failed = faExclamationTriangle;
+  delete = faTimes;
 
-  constructor() { }
-
+  constructor() {}
 }
