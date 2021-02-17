f23aa65da3daba1ad8b930d349ff9bc6e6bc39e0
berezkin88 <berezkin05@gmail.com>
2021-02-18 01:47:37 +0200

    stroke first test

--- src/app/bills-acts/bills-acts.component.spec.ts
+++ src/app/bills-acts/bills-acts.component.spec.ts
@@ -1,4 +1,4 @@
-import { EMPTY, Observable } from 'rxjs';
+import { EMPTY, NEVER, Observable } from 'rxjs';
 import { AuthService } from './../services/auth.service';
 import { TenantsService } from './../services/tenants.service';
 import { DocsService } from './../services/docs.service';
@@ -9,12 +9,16 @@ import { BillsActsComponent } from './bills-acts.component';
 describe('BillsActsComponent', () => {
   let component: BillsActsComponent;
   let fixture: ComponentFixture<BillsActsComponent>;
+  let docsService: jasmine.SpyObj<DocsService>;
 
   beforeEach(async () => {
+    docsService = jasmine.createSpyObj('DocsService', ['getAllDocs']);
+    docsService.getAllDocs.withArgs(undefined).and.returnValue(EMPTY);
+
     await TestBed.configureTestingModule({
       declarations: [ BillsActsComponent ],
       providers: [
-        { provide: DocsService, useClass: MockDocsService },
+        { provide: DocsService, useValue: docsService },
         { provide: TenantsService, useClass: MockTenantService },
         { provide: AuthService, useClass: MockAuthService },
         BillsActsComponent
@@ -31,6 +35,12 @@ describe('BillsActsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it ('should call services', () => {
+    docsService.getAllDocs.withArgs(undefined).and.returnValue(EMPTY);
+    component.ngOnInit();
+    expect(docsService.getAllDocs).toHaveBeenCalled();
   });
 });
 
