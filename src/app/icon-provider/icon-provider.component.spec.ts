450f056e1f4aa52f767c2d67808c488aa183440c
berezkin88 <berezkin05@gmail.com>
2021-02-21 00:17:03 +0200

    covered icon-provider with tests

--- src/app/icon-provider/icon-provider.component.spec.ts
+++ src/app/icon-provider/icon-provider.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { faClipboardCheck, faExclamationTriangle, faHourglassHalf, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { Status } from '../models/status';
 
 import { IconProviderComponent } from './icon-provider.component';
 
@@ -21,5 +23,33 @@ describe('IconProviderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have complete icon', () => {
+    component.status = Status.COMPLETE;
+    component.ngOnInit();
+    expect(component.icon).toEqual(faClipboardCheck);
+    expect(component.iconColor).toEqual('icon-green');
+  });
+
+  it('should have pending icon', () => {
+    component.status = Status.PENDING;
+    component.ngOnInit();
+    expect(component.icon).toEqual(faHourglassHalf);
+    expect(component.iconColor).toEqual('icon-grey');
+  });
+
+  it('should have complete icon', () => {
+    component.status = Status.FAILED;
+    component.ngOnInit();
+    expect(component.icon).toEqual(faExclamationTriangle);
+    expect(component.iconColor).toEqual('icon-red');
+  });
+
+  it('should have unknown icon', () => {
+    component.status = Status.UNKNOWN;
+    component.ngOnInit();
+    expect(component.icon).toEqual(faQuestionCircle);
+    expect(component.iconColor).toEqual('icon-red');
   });
 });
