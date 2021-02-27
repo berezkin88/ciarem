ee2ad9ceea50332874e706e2312551dc5d6e4701
berezkin88 <berezkin05@gmail.com>
2021-02-27 23:01:42 +0200

    built act-reconciliation component, extracted orange button css

--- src/app/act-reconciliation/act-reconciliation.component.ts
+++ src/app/act-reconciliation/act-reconciliation.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'act-reconciliation',
   templateUrl: './act-reconciliation.component.html',
   styleUrls: ['./act-reconciliation.component.sass']
 })
-export class ActReconciliationComponent implements OnInit {
+export class ActReconciliationComponent {
+  dateFrom: string;
+  dateTo: string;
 
-  constructor() { }
+  constructor() {
+    this.dateFrom = new Date().toISOString().split('T')[0];
+    this.dateTo = new Date().toISOString().split('T')[0];
+   }
 
-  ngOnInit(): void {
-  }
-
+   submitRequest(): void {
+     console.log('Fake function');
+     console.log(this.dateFrom);
+     console.log(this.dateTo);
+   }
 }
