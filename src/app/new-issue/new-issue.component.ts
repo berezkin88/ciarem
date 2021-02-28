b7fda851245c8b04347ca16dde57b85378692d4a
berezkin88 <berezkin05@gmail.com>
2021-02-28 23:47:35 +0200

    completed new-issue component

--- src/app/new-issue/new-issue.component.ts
+++ src/app/new-issue/new-issue.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'new-issue',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-issue.component.sass']
 })
 export class NewIssueComponent implements OnInit {
+  issueNumber: string;
 
-  constructor() { }
+  constructor() {
+    this.issueNumber = this.getRandom();
+   }
 
   ngOnInit(): void {
   }
 
+  private getRandom(): string {
+    return  Math.floor(Math.random() * Math.floor(999)) as unknown as string;
+  }
+
+  submit(value: any): void {
+    console.log('This is a fake function');
+    console.log(value);
+  }
 }
