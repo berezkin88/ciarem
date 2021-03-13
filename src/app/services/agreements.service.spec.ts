eb3db084428c089664d092871aeac9a00e766f2c
berezkin88 <berezkin05@gmail.com>
2021-03-13 16:21:02 +0200

    added and fixed tests for agreements service

--- src/app/services/agreements.service.spec.ts
+++ src/app/services/agreements.service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { EMPTY } from 'rxjs';
 
 import { AgreementsService } from './agreements.service';
 
 describe('AgreementsService', () => {
   let service: AgreementsService;
+  let mockDB: jasmine.SpyObj<AngularFireDatabase>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockDB = jasmine.createSpyObj('AngularFireDatabase', ['list']);
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{ provide: AngularFireDatabase, useValue: mockDB }],
+    });
     service = TestBed.inject(AgreementsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return Observable', () => {
+    const mockFirebaseList = jasmine.createSpyObj('AngularFireList', [
+      'snapshotChanges',
+    ]);
+
+    mockDB.list.and.returnValue(mockFirebaseList);
+    mockFirebaseList.snapshotChanges.and.returnValue(EMPTY);
+    const actualValue = service.getAll();
+
+    expect(actualValue).toBeTruthy();
   });
 });
